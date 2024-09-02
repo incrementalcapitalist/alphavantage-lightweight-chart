3229b19748d7074cf07aed18d95ca5e6e80a0e63
incremental capital <incrementalcapital@gmail.com>
2024-09-02 19:29:02 +0900

    remove error creating code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import type { AppProps } from 'next/app';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
-import outputs from '@/amplify_outputs.json';
 import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
