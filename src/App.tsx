597683bf477378197b0547d53aa1ca19600bc25f
incremental capital <incrementalcapital@gmail.com>
2024-09-04 20:07:37 +0900

    can't be done with a custom authenticator via the console!

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 /**
  * @file App.tsx
- * @version 1.5.2
- * @description Main application component with updated theme usage
+ * @version 3.0.0
+ * @description Main application component using default Amplify Authenticator
  */
 
 // Import necessary dependencies
 import React from "react";
-import { Authenticator, ThemeProvider } from '@aws-amplify/ui-react';
+import { Authenticator, ThemeProvider, View, Heading, Button } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 import StockQuote from "./components/StockQuote";
@@ -32,24 +32,24 @@ const App: React.FC = () => {
       <Authenticator> 
         {/* Wrap the entire app an with Authenticator component for user authentication */}
         {({ signOut, user }) => (
-          <div className="min-h-screen bg-gray-900 py-6 flex flex-col justify-center sm:py-12">
-            <div className="px-4 sm:px-6 lg:px-8">
-              <div className="max-w-md mx-auto">
-                <h1 className="text-3xl sm:text-4xl font-bold text-center text-purple-300 mb-8">
-                  Alpha Vantage Stock Quotation App
-                </h1>
+          <View className="min-h-screen bg-gray-900 py-6 flex flex-col justify-center sm:py-12">
+            <View className="px-4 sm:px-6 lg:px-8">
+              <View className="max-w-md mx-auto">
+                <Heading level={1} className="text-3xl sm:text-4xl font-bold text-center text-purple-300 mb-8">
+                  AlphaVantage Lightweight Charts
+                </Heading>
                 <StockQuote />
                 {signOut && (
-                  <button
+                  <Button
                     onClick={signOut}
                     className="mt-8 w-full bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
                   >
                     Sign Out
-                  </button>
+                  </Button>
                 )}
-              </div>
-            </div>
-          </div>
+              </View>
+            </View>
+          </View>
         )}
       </Authenticator>
     </ThemeProvider>
